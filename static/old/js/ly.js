da8245df37228b0853d269dc6393f63ac8b50981
凌云 <109946426+YUANXINGlingyun@users.noreply.github.com>
2023-05-21 19:32:03 +0800

    推一波更新，移动牌子到spigotserver里去（热铁盒开始测试）

--- static/old/js/ly.js
+++ static/old/js/ly.js
@@ -6,14 +6,26 @@ xhr.onreadystatechange = function() {
         const result = response.match(pattern);
         if (result[1]>0) {
             console.log('Result->'+result[1])
-                document.getElementById("count").dataset.number = result[1];
+                document.getElementById("count1").innerText = result[1];
         } else {
-            console.log("失败");
-            console.log('Result->'+result[1])
-                document.getElementById("count").dataset.number = "failed";
+            console.log('失败：Result->'+result[1])
         }
     }
 };
 
-xhr.open('GET','https://mcapi.us/server/status?ip=mc.byboy.top&port=25565',true);
+$.ajax({
+    url: 'https://gitee.com/JingHai-Lab/mc521-website-v2/raw/master/countnumber.txt',
+    type: 'GET',
+    success: function(response) {
+        const { serverjars, clientmods } = response;
+        $('#count2').text(serverjars);
+        $('#count3').text(clientmods);
+        console.log(serverjars, clientmods);
+    },
+    error: function(xhr, status, error) {
+        console.log('XHR 加载失败：', error);
+    }
+});
+
+xhr.open('GET','https://mcapi.us/server/status?ip=mc521.cc&port=25565',true);
 xhr.send()
--- static/old/js/number.js
+++
@@ -1,4 +0,0 @@
-var firstzhoumu = 1657;
-var secondzhoumu = 444;
-var serverplugins = 72;
-var clientmods = 14;
